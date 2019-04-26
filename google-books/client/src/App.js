ccb2acf6d99003c16896f1f10851a6a136a31cb4
reubenwalker64 <rwalker@mobile-atom.com>
2019-04-25 20:19:49 -0400

    changes from class demo

--- google-books/client/src/App.js
+++ google-books/client/src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from react-router-dom;
 import './App.css';
+import search from "/pages/search";
+import saved from "/pages/saved";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Find your book of the day.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Google Book Search
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Google Books Search</h1>
+            <p>
+              Find your book of the day.
+            </p>
+            <a
+              className="App-link"
+              href="https://???"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Google Book Search
+            </a>
+          </header>
+        </div>
+        <div>
+          <Route path="/" component="search" />
+          <Route path="/saved" component="saved" />
+        </div>
+      </Router>
     );
   }
 }
